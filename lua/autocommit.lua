ba9983c773cfdee788c826c8eecc1bad356b5321
crhowell3 <crhowell3@users.noreply.github.com>
2023-04-13 21:24:53 +0000

    chore: autogen (vimdocs+formatting)

--- lua/autocommit.lua
+++ lua/autocommit.lua
@@ -10,28 +10,28 @@ local function autocommit()
   local bufname = api.nvim_buf_get_name(0)
 
   -- Check if the buffer is in a Git repository
-  local git_dir_cmd = "git -C " .. vim.fn.expand("%:p:h") .. " rev-parse --git-dir"
+  local git_dir_cmd = 'git -C ' .. vim.fn.expand('%:p:h') .. ' rev-parse --git-dir'
   local git_dir_result = vim.fn.systemlist(git_dir_cmd)
   local git_dir = git_dir_result[1]
-  if git_dir == nil or git_dir == "" then
+  if git_dir == nil or git_dir == '' then
     return
   end
 
   -- Get the relative file path
-  local file_path = vim.fn.fnamemodify(bufname, ":.")
+  local file_path = vim.fn.fnamemodify(bufname, ':.')
 
   -- Add changes to Git index
-  local git_add_cmd = "git -C " .. vim.fn.expand("%:p:h") .. " add " .. file_path
+  local git_add_cmd = 'git -C ' .. vim.fn.expand('%:p:h') .. ' add ' .. file_path
   vim.fn.system(git_add_cmd)
 
   -- Commit changes with a message
-  local commit_msg = string.format("Autocommit: %s", file_path)
-  local git_commit_cmd = "git -C " .. vim.fn.expand("%:p:h") .. " commit -m '" .. commit_msg .. "'"
+  local commit_msg = string.format('Autocommit: %s', file_path)
+  local git_commit_cmd = 'git -C ' .. vim.fn.expand('%:p:h') .. " commit -m '" .. commit_msg .. "'"
   vim.fn.system(git_commit_cmd)
 end
 
 M = {
-  autocommit = autocommit
+  autocommit = autocommit,
 }
 
 return M
