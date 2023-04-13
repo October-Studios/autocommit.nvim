c0807db8dfb85165c13c467b938e9bb47b08b708
Cameron Howell <cameron.howell@ams3sx.com>
2023-04-13 16:08:59 -0500

    Testing something

--- lua/autocommit.lua
+++ lua/autocommit.lua
@@ -5,39 +5,33 @@
 local api = vim.api
 local M
 
-local function setup()
-  -- Define Git commit message template
-  local commit_template = "Auto-commit: Save %s"
+local function autocommit()
+  -- Get the current buffer name
+  local bufname = api.nvim_buf_get_name(0)
 
-  -- Define autocommit function
-  local function autocommit()
-    -- Get current buffer name
-    local bufname = api.nvim_buf_get_name(0)
-
-    -- Check if buffer is in a Git repository
-    local git_dir_cmd = "git -C " .. vim.fn.expand("%:p:h") .. " rev-parse --git-dir"
-    local git_dir_result = vim.fn.systemlist(git_dir_cmd)
-    local git_dir = git_dir_result[1]
-    if git_dir == nil or git_dir == "" then
-      return
-    end
-
-    -- Get the relative file path
-    local file_path = vim.fn.fnamemodify(bufname, ":.")
-
-    -- Add changes to Git index
-    local git_add_cmd = "git -C " .. vim.fn.expand("%:p:h") .. " add " .. file_path
-    vim.fn.system(git_add_cmd)
-
-    -- Commit changes with a message
-    local commit_msg = string.format(commit_template, file_path)
-    local git_commit_cmd = "git -C " .. vim.fn.expand("%:p:h") .. " commit -m '" .. commit_msg .. "'"
-    vim.fn.system(git_commit_cmd)
+  -- Check if the buffer is in a Git repository
+  local git_dir_cmd = "git -C " .. vim.fn.expand("%:p:h") .. " rev-parse --git-dir"
+  local git_dir_result = vim.fn.systemlist(git_dir_cmd)
+  local git_dir = git_dir_result[1]
+  if git_dir == nil or git_dir == "" then
+    return
   end
+
+  -- Get the relative file path
+  local file_path = vim.fn.fnamemodify(bufname, ":.")
+
+  -- Add changes to Git index
+  local git_add_cmd = "git -C " .. vim.fn.expand("%:p:h") .. " add " .. file_path
+  vim.fn.system(git_add_cmd)
+
+  -- Commit changes with a message
+  local commit_msg = string.format("Autocommit: %s", file_path)
+  local git_commit_cmd = "git -C " .. vim.fn.expand("%:p:h") .. " commit -m '" .. commit_msg .. "'"
+  vim.fn.system(git_commit_cmd)
 end
 
 M = {
-  setup = setup
+  autocommit = autocommit
 }
 
 return M
